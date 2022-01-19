72264810fadda26e6639f4cf8740356ee5a7a3a4
AgosBC <agostinabarello@gmail.com>
2022-01-19 19:57:17 -0300

    create category entity and its attributes

---
+++ src/main/java/com/alkemy/ong/model/entity/Category.java
@@ -0,0 +1,20 @@
+package com.alkemy.ong.model.entity;
+
+import java.security.Timestamp;
+
+public class Category {
+
+    private Integer categoryId;
+
+    private String name;
+
+    private String description; //nullable
+
+    private String image; //nullable
+
+    private Timestamp timestamp;
+
+    private boolean softDelete;
+
+    
+}
