a353c96a90ed02bba36156b7f00f0e02e41b1665
Facundo Castro <facucastrolp123@gmail.com>
2022-01-20 12:11:44 -0300

    correction of sintaxys and TimeStamp

--- src/main/java/com/alkemy/ong/model/entity/Role.java
+++ src/main/java/com/alkemy/ong/model/entity/Role.java
@@ -1,21 +1,19 @@
 package com.alkemy.ong.model.entity;
 
-import java.time.LocalDate;
-import java.util.Calendar;
+import java.sql.Timestamp;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
-@Table(name = "Roles")
+@Table(name = "ROLES")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -24,16 +22,16 @@ public class Role {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "ID_ROLE")
-  private Long ID_ROLE;
+  private Long Id;
 
   @Column(name = "NAME", nullable = false)
-  private String NAME;
+  private String Name;
 
   @Column(name = "DESCRIPTION", nullable = true)
-  private String DESCRIPTION;
+  private String Description;
 
+  @CreationTimestamp
   @Column(name = "ROLE_TIMESTAMP")
-  @Temporal(TemporalType.TIMESTAMP)
-  private Calendar ROLE_TIMESTAMP;
+  private Timestamp TimeStamp;
 
 }
