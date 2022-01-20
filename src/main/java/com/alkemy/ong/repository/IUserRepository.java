0cc81ea87acff5ef23cef135012cd14453dc908b
intisolar <insolgar@gmail.com>
2022-01-20 20:02:00 -0300

    Created User entity and its repository

---
+++ src/main/java/com/alkemy/ong/repository/IUserRepository.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.alkemy.ong.repository;
+
+import com.alkemy.ong.model.entity.Category;
+import com.alkemy.ong.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUserRepository extends JpaRepository<User, Long> {
+
+
+}
