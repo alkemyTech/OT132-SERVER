6bdd7dc129b71cde4374d216186c001fb478c2c1
AgosBC <agostinabarello@gmail.com>
2022-01-20 11:17:18 -0300

    OT132-22 crate: entity member and its attributes

---
+++ src/main/java/com/alkemy/ong/model/entity/Member.java
@@ -0,0 +1,25 @@
+package com.alkemy.ong.model.entity;
+
+import java.security.Timestamp;
+
+public class Member {
+
+    private Integer memberId;
+
+    private String name;
+
+    private String facebookUrl;
+
+    private String instagramUrl;
+
+    private String linkedinUrl;
+
+    private String image;
+
+    private String description;
+
+    private Timestamp timestamp;
+
+    private boolean softDelete;
+    
+}
