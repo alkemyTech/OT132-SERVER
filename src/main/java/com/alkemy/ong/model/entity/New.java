83252c2f7bcf2a462150cd3b0416787e108f0d36
Alejo Galizzi <alejogalizzi@gmail.com>
2022-01-20 13:37:09 -0300

    OT132-19: New entity created with his attributes and getters and setters using lombok libraries

---
+++ src/main/java/com/alkemy/ong/model/entity/New.java
@@ -0,0 +1,43 @@
+package com.alkemy.ong.model.entity;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "news")
+@Getter
+@Setter
+public class New {
+
+    @Column(name = "NEW_ID")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @Column(name = "TEXT", nullable = false)
+    private String text;
+
+    @Column(name = "IMAGE", nullable = false)
+    private String image;
+
+    @Column(name = "SOFT_DELETE")
+    private boolean softDelete;
+
+    @Column(name = "NEW_TIMESTAMP")
+    @CreationTimestamp
+    private Timestamp timestamp;
+}
