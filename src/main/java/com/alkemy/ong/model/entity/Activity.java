f2a880b863d60ecf134c25d30d4b9808f84c73d1
Facundo Besse <facundobesse@gmail.com>
2022-01-20 20:04:57 -0300

    entity corrected

--- src/main/java/com/alkemy/ong/model/entity/Activity.java
+++ src/main/java/com/alkemy/ong/model/entity/Activity.java
@@ -10,32 +10,37 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.Data;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "activities")
+@Table(name = "ACTIVITIES")
 public class Activity {
   @Id
+	@Column(name = "ACTIVITY_ID")
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+  private Long activityId;
 
-
-	@Column(name = "NAMES", nullable = false)
+	@Column(name = "NAME", nullable = false)
 	private String name;
 
-	@Column(name = "CONTENTS", nullable = false)
+	@Column(name = "CONTENT", nullable = false)
 	private String content;
 
-	@Column(name = "IMAGES", nullable = false)
+	@Column(name = "IMAGE", nullable = false)
 	private String image;
 
-	@Column(name = "TIMESTAMPS")
-	private Timestamp timestap;		
+	@Column(name = "TIMESTAMP")
+	@CreationTimestamp
+	private Timestamp timestamp;		
 
-	@Column(name = "SOFT_DELETES")
-	private Boolean softDelete;
+	@Column(name = "SOFT_DELETE")
+	private boolean softDelete;
 
 }
