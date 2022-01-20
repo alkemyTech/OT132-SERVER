8a4b382ecfdb154a63a32876b800141870564c26
Franco Lorca <lorcafrancog@gmail.com>
2022-01-20 18:49:09 -0300

    OT132-53:
    Added entity Contact with its repository.

---
+++ src/main/java/com/alkemy/ong/model/entity/Contact.java
@@ -0,0 +1,40 @@
+package com.alkemy.ong.model.entity;
+
+import java.sql.Timestamp;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "CONTACTS")
+public class Contact {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "CONTACT_ID", nullable = false)
+  private Long contactId;
+
+  @Column(name = "NAME", nullable = false)
+  private String name;
+
+  @Column(name = "PHONE", nullable = false)
+  private Integer phone;
+
+  @Column(name = "EMAIL", nullable = false)
+  private String email;
+
+  @Column(name = "MESSAGE", nullable = false)
+  private String message;
+
+  @Column(name = "DELETE_AT")
+  @CreationTimestamp
+  private Timestamp deleteAt;
+}
