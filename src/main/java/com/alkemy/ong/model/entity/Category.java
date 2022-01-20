8df9ec08af2cb9d9d04fb95093c6a4dff817ac15
AgosBC <agostinabarello@gmail.com>
2022-01-19 21:06:01 -0300

    add anotations. find problem when trying to use lombok anotation @Getter @Setter

--- src/main/java/com/alkemy/ong/model/entity/Category.java
+++ src/main/java/com/alkemy/ong/model/entity/Category.java
@@ -2,19 +2,35 @@ package com.alkemy.ong.model.entity;
 
 import java.security.Timestamp;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categories")
 public class Category {
 
+    @Column(name = "CATEGORY_ID")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer categoryId;
 
     private String name;
 
-    private String description; //nullable
+    @Column(nullable = true)
+    private String description;
 
-    private String image; //nullable
+    @Column(nullable = true)
+    private String image;
 
     private Timestamp timestamp;
 
     private boolean softDelete;
 
-    
 }
