813786d9669698b6da147694c5749ad73a80e2b2
facubc <79551405+facubc@users.noreply.github.com>
2022-01-20 20:13:30 -0300

    OT132-21: create model and repository for activities (#7)

---
+++ src/main/java/com/alkemy/ong/repository/IActivityRepository.java
@@ -0,0 +1,12 @@
+
+package com.alkemy.ong.repository;
+
+import com.alkemy.ong.model.entity.Activity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IActivityRepository extends JpaRepository<Activity, Long> {
+
+}
+
