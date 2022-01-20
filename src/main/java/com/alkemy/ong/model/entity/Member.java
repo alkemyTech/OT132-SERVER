6490e40d33519be6a0624a54154715caabfe70b6
AgosBC <agostinabarello@gmail.com>
2022-01-20 11:53:16 -0300

    OT132-22 add javax persistance anotation

--- src/main/java/com/alkemy/ong/model/entity/Member.java
+++ src/main/java/com/alkemy/ong/model/entity/Member.java
@@ -2,24 +2,46 @@ package com.alkemy.ong.model.entity;
 
 import java.security.Timestamp;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Table(name = "MEMBERS")
 public class Member {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MEMBER_ID")
     private Integer memberId;
 
+    @Column(name = "NAME", nullable = false)
     private String name;
 
+    @Column(name = "FACEBOOK_URL")
     private String facebookUrl;
 
+    @Column(name = "INSTAGRAM_URL")
     private String instagramUrl;
 
+    @Column(name = "LINKEDIN_URL")
     private String linkedinUrl;
 
+    @Column(name = "IMAGE", nullable = false)
     private String image;
 
+    @Column(name = "DESCRIPTION")
     private String description;
 
+    @Column(name = "TIMESTAMP", nullable = false)
     private Timestamp timestamp;
 
+    @Column(name = "SOFT_DELETE")
     private boolean softDelete;
-    
+
 }
