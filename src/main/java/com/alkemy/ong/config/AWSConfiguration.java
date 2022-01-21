43799ca374f91c732c2a924007591d430e4ef479
AgosBC <agostinabarello@gmail.com>
2022-01-21 15:22:40 -0300

    OT132-38 DELETE unnecessary imports

--- src/main/java/com/alkemy/ong/config/AWSConfiguration.java
+++ src/main/java/com/alkemy/ong/config/AWSConfiguration.java
@@ -1,12 +1,9 @@
 package com.alkemy.ong.config;
 
-import javax.annotation.PostConstruct;
-import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
