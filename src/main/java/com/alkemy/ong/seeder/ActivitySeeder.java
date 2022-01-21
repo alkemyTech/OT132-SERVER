fa7b5918c0a984ce99d3a532d19a09c0542a49e5
AlejoGalizzi <alejogalizzi@gmail.com>
2022-01-21 11:48:44 -0300

    OT132-52: Logic of the seedActivityTable created. It will create 3 entities and will save it in the database.

--- src/main/java/com/alkemy/ong/seeder/ActivitySeeder.java
+++ src/main/java/com/alkemy/ong/seeder/ActivitySeeder.java
@@ -5,20 +5,41 @@ import com.alkemy.ong.repository.IActivityRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ActivitySeeder implements CommandLineRunner {
 
-    @Autowired 
+    @Autowired
     IActivityRepository iActivityRepository;
 
     @Override
     public void run(String... args) throws Exception {
-        // TODO Auto-generated method stub
-        
-    }
-    
-    private void seedActivityTable(){
+        seedActivityTable();
 
+    }
+
+    private void seedActivityTable() {
+        if (iActivityRepository.count() == 0) {
+            Activity act1 = new Activity();
+            act1.setName("School support for the primary level");
+            act1.setContent(
+                    "The workshops are held from Monday to Thursday from 10 a.m. to 12 p.m. and from 2 p.m. to 4 p.m. in the counter shift.");
+            act1.setImage("-");
+            iActivityRepository.save(act1);
+            Activity act2 = new Activity();
+            act2.setName("Secondary school support");
+            act2.setContent(
+                    "The workshops are held from Monday to Friday from 10 a.m. to 12 p.m. and from 4 p.m. to 6 p.m. in the counter shift.");
+            act2.setImage("-");
+            iActivityRepository.save(act2);
+            Activity act3 = new Activity();
+            act3.setName("Tutorials");
+            act3.setContent(
+                    "Its objective is to guarantee permanence in school and build a life project that gives meaning to school.");
+            act3.setImage("-");
+            iActivityRepository.save(act3);
+        }
     }
 
 }
