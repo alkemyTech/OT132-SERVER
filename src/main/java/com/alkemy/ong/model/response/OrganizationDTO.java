fca00a92ff251f529fc6702a02fcf9a1f3cfb723
Facundo Besse <facundobesse@gmail.com>
2022-01-22 20:23:25 -0300

    organization DTO created

---
+++ src/main/java/com/alkemy/ong/model/response/OrganizationDTO.java
@@ -0,0 +1,16 @@
+
+package com.alkemy.ong.model.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrganizationDTO {
+	
+	private String name;
+	private String image;
+	private String address;
+	private Integer phone;
+
+}
