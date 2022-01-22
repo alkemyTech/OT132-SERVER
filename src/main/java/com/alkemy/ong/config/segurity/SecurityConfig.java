9d58b3dff3094a4b1f6c9615df6421c4f67ec200
Alejo Galizzi <alejogalizzi@gmail.com>
2022-01-22 17:37:28 -0300

    OT132-73: Request filter added to the configure method.

--- src/main/java/com/alkemy/ong/config/segurity/SecurityConfig.java
+++ src/main/java/com/alkemy/ong/config/segurity/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -51,6 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .and()
         .authorizeRequests()
         .anyRequest().permitAll();
+    http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
   }
 
 }
