65b5818dd8f425c25d7a9444b221ab78c301ce7e
Vladimir Morote <46836725+VladimirMorote@users.noreply.github.com>
2022-01-22 14:01:32 -0300

    corregido

--- src/main/java/com/alkemy/ong/config/segurity/SegurityConfig.java
+++ src/main/java/com/alkemy/ong/config/segurity/SegurityConfig.java
@@ -1,97 +1,14 @@
 package com.alkemy.ong.config.segurity;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 public class SegurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private BCryptPasswordEncoder passwordEncoder;
-
-    @Autowired
-    private UserDetailsServices userDetailsService;
-
-    @Autowired
-    private JwtFilter jwtFilter;
-
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests()
-                .antMatchers("/auth/**",
-                        "/register",
-                        "/login",
-                        "/v3/api-docs/swagger-config",
-                        "/api/swagger-ui/**",
-                        "/v3/api-docs",
-                        "/api/docs",
-                        "/api/docs/**",
-                        "/contacts")
-                .permitAll()
-                .antMatchers(HttpMethod.GET,
-                        "/categories/{id}",
-                        "/categories",
-                        "/s3/images",
-                        "/organization/public",
-                        "/organization/public/{id}",
-                        "/contacts",
-                        "/users",
-                        "/testimonials").hasAnyAuthority("ADMIN","USER")
-                .antMatchers(HttpMethod.GET,
-                        "/news**").hasAuthority("USER")
-                .antMatchers(HttpMethod.GET,
-                        "/members",
-                        "/comments").hasAuthority("ADMIN")
-                .antMatchers(HttpMethod.POST,
-                        "/testimonials",
-                        "/members",
-                        "/categories",
-                        "/s3/images",
-                        "/organization/public",
-                        "/contacts").hasAuthority("ADMIN")
-                .antMatchers(HttpMethod.PUT,
-                        "/testimonials",
-                        "/members/{id}",
-                        "/categories/{id}").hasAuthority("ADMIN")
-                .antMatchers(HttpMethod.PUT,
-                        "comments/{id}").hasAnyAuthority("ADMIN","USER")
-                .antMatchers(HttpMethod.PATCH,
-                        "/organization/public/{id}",
-                        "/users/{id}").hasAuthority("ADMIN")
-                .antMatchers(HttpMethod.DELETE,
-                        "/testimonials/{id}",
-                        "/members/{id}",
-                        "/categories/{id}",
-                        "/users/{id}").hasAuthority("ADMIN")
-                .antMatchers(HttpMethod.DELETE,
-                        "/comments/{id}").hasAnyAuthority("ADMIN","USER")
-                .anyRequest().authenticated().and().sessionManagement();
-
-        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         @Bean
         @Override
         public AuthenticationManager authenticationManagerBean() throws Exception {
             return super.authenticationManagerBean();
         }
-
-        @Bean
-        public DaoAuthenticationProvider authProvider() {
-            DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-            authProvider.setUserDetailsService(userDetailsService);
-            authProvider.setPasswordEncoder(passwordEncoder);
-            return authProvider;
-        }
-
-
-    }
-
 }
