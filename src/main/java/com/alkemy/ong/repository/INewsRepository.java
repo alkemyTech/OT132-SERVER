a4420e8411b5dd9ffd3111edfd2db1ce2cb5bcff
Magali Kain <magali.kain@thoughtworks.com>
2022-01-24 19:57:14 +0100

    Added missing repository annotation to be consistent with the code in the project

--- src/main/java/com/alkemy/ong/repository/INewsRepository.java
+++ src/main/java/com/alkemy/ong/repository/INewsRepository.java
@@ -2,7 +2,9 @@ package com.alkemy.ong.repository;
 
 import com.alkemy.ong.model.entity.News;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface INewsRepository extends JpaRepository<News, Long> {
 
 }
