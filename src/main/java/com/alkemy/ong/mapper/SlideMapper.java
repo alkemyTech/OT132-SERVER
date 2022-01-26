57f51e5a4035c2a3a6ffca4742dba1c5765089fd
AlejoGalizzi <81248632+AlejoGalizzi@users.noreply.github.com>
2022-01-26 17:43:59 -0300

    OT132-60: add slides to public endpoint (#20)

---
+++ src/main/java/com/alkemy/ong/mapper/SlideMapper.java
@@ -0,0 +1,29 @@
+package com.alkemy.ong.mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.alkemy.ong.model.entity.Slide;
+import com.alkemy.ong.model.response.SlideResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SlideMapper {
+
+    public SlideResponse map(Slide slide) {
+        SlideResponse slideResponse = new SlideResponse();
+        slideResponse.setImageUrl(slide.getImageUrl());
+        slideResponse.setText(slide.getText());
+        slideResponse.setOrder(slide.getOrder());
+        return slideResponse;
+    }
+
+    public List<SlideResponse> mapList(List<Slide> slides) {
+        List<SlideResponse> slideResponses = new ArrayList<SlideResponse>(slides.size());
+        for (Slide slide : slides) {
+            slideResponses.add(this.map(slide));
+        }
+        return slideResponses;
+    }
+}
