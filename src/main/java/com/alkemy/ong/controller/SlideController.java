09a13fd85126bd7e1518e414582c9db998478472
Facundo Castro <facucastrolp123@gmail.com>
2022-01-26 17:53:49 -0300

    OT132-55 Changes in SlideController

--- src/main/java/com/alkemy/ong/controller/SlideController.java
+++ src/main/java/com/alkemy/ong/controller/SlideController.java
@@ -1,17 +1,23 @@
 package com.alkemy.ong.controller;
 
 import com.alkemy.ong.model.response.SlideResponse;
+import com.alkemy.ong.service.SlideService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("Slides")
+@RequestMapping("slides")
 public class SlideController {
 
-@GetMapping
-    public ResponseEntity<SlideResponse> getImageOrder(){
+    @Autowired
+    SlideService slideService;
 
-}
+    @GetMapping
+    public ResponseEntity<List<SlideResponse>> getImageOrder() {
+        List<SlideResponse> response = slideService.findAll();
+        return ResponseEntity.ok().body(response);
+    }
 }
