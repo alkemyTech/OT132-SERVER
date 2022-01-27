e3d902513c4714495e79fc437fe69bb2f4985f04
Alejo Galizzi <alejogalizzi@gmail.com>
2022-01-27 17:43:45 -0300

    enum: Fixed.

--- src/main/java/com/alkemy/ong/common/RoleEnum.java
+++ src/main/java/com/alkemy/ong/common/RoleEnum.java
@@ -2,13 +2,4 @@ package com.alkemy.ong.common;
 
 public enum RoleEnum {
 	ADMIN, USER;
-
-	public String getRole() {
-		switch (this) {
-			case ADMIN:
-				return "ADMIN";
-			default:
-				return "USER";
-		}
-	}
 }
