5217f6261e52b8edc3abbe9e0489b832dc594bf3
Vladimir Morote <46836725+VladimirMorote@users.noreply.github.com>
2022-01-27 14:12:31 -0300

    prueba

---
+++ src/main/java/com/alkemy/ong/controller/MemberController.java
@@ -0,0 +1,16 @@
+package com.alkemy.ong.controller;
+
+import com.alkemy.ong.model.entity.Member;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/members")
+public class MemberController {
+
+    @GetMapping
+    public ResponseEntity<Me>
+
+}
