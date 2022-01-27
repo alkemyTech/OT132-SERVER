aa42b6fc016c5ba559f187a39ed49e4d979a13d0
Facundo Castro <facucastrolp123@gmail.com>
2022-01-26 21:19:21 -0300

    OT132-55 Include annotations for validate role

--- src/main/java/com/alkemy/ong/controller/SlideController.java
+++ src/main/java/com/alkemy/ong/controller/SlideController.java
@@ -4,6 +4,8 @@ import com.alkemy.ong.model.response.SlideResponse;
 import com.alkemy.ong.service.abstraction.IGetSlideDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.jaas.AuthorityGranter;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,7 +17,8 @@ public class SlideController {
     @Autowired
     private IGetSlideDetails iGetSlideDetails;
 
-    @GetMapping("/")
+    @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     public ResponseEntity<List<SlideResponse>> getImageOrder() {
         List<SlideResponse> response = iGetSlideDetails.findImageOrder();
         return ResponseEntity.ok().body(response);
--- src/main/java/com/alkemy/ong/model/response/SlideResponse.java
+++ src/main/java/com/alkemy/ong/model/response/SlideResponse.java
@@ -1,6 +1,7 @@
 package com.alkemy.ong.model.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Getter;
 import lombok.Setter;
 
