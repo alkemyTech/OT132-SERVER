4156f1c590a0a5daa4094ff0354aa27e54754e1b
Alejo Galizzi <alejogalizzi@gmail.com>
2022-01-27 23:02:11 -0300

    OT132-39: UploadImageService and his abstraction modified in orde to use the handle expetion with the classes created before.

--- src/main/java/com/alkemy/ong/service/UploadImageService.java
+++ src/main/java/com/alkemy/ong/service/UploadImageService.java
@@ -1,40 +1,37 @@
 package com.alkemy.ong.service;
 
-import java.io.InputStream;
-
+import com.alkemy.ong.exception.UploadErrorException;
 import com.alkemy.ong.service.abstraction.IUploadImageUrl;
+
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.io.InputStream;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
 public class UploadImageService implements IUploadImageUrl {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(UploadImageService.class);
+  @Value("${amazonProperties.bucket}")
+  private String bucketName;
 
-    @Value("${amazonProperties.bucket}")
-    private String bucketName;
+  @Value("${amazonProperties.endpointURL}")
+  private String endpointUrl;
 
-    @Value("${amazonProperties.endpointURL}")
-    private String endpointURL;
+  @Autowired
+  private AmazonS3 amazonS3;
 
-    @Autowired
-    private AmazonS3 amazonS3;
-
-    @Override
-    public String uploadImage(InputStream input, ObjectMetadata contentType, String fileName) {
-        try {
-            PutObjectRequest request = new PutObjectRequest(bucketName, fileName, input, contentType);
-            LOGGER.info("Subiendo archivo con el nombre..." + fileName);
-            amazonS3.putObject(request);
-        } catch (Exception e) {
-            LOGGER.error(e.getMessage(), e);
-        }
-        return String.valueOf(amazonS3.getUrl(bucketName, (endpointURL + fileName)));
+  @Override
+  public String uploadImage(InputStream input, ObjectMetadata contentType, String fileName) {
+    try {
+      PutObjectRequest request = new PutObjectRequest(bucketName, fileName, input, contentType);
+      amazonS3.putObject(request);
+    } catch (Exception e) {
+      throw new UploadErrorException(e.getMessage());
     }
+    return String.valueOf(amazonS3.getUrl(bucketName, (endpointUrl + fileName)));
+  }
 
 }
--- src/main/java/com/alkemy/ong/service/abstraction/IUploadImageUrl.java
+++ src/main/java/com/alkemy/ong/service/abstraction/IUploadImageUrl.java
@@ -1,10 +1,12 @@
 package com.alkemy.ong.service.abstraction;
 
+import com.amazonaws.services.s3.model.ObjectMetadata;
+
 import java.io.InputStream;
 
-import com.amazonaws.services.s3.model.ObjectMetadata;
+
 
 public interface IUploadImageUrl {
 
-    String uploadImage(InputStream input, ObjectMetadata contentType, String fileName);
+  String uploadImage(InputStream input, ObjectMetadata contentType, String fileName);
 }
