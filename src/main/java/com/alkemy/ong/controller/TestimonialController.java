4043a4e9b2627f77c854070de80ec68b3e7597f2
Facundo Castro <facucastrolp123@gmail.com>
2022-01-27 22:20:56 -0300

    OT132-65 Add TestimonialController, TestimonialResponse

---
+++ src/main/java/com/alkemy/ong/controller/TestimonialController.java
@@ -0,0 +1,17 @@
+package com.alkemy.ong.controller;
+
+import com.alkemy.ong.model.entity.Testimonial;
+import java.sql.Timestamp;
+import javax.persistence.Column;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("testimonials")
+public class TestimonialController {
+
+
+}
---
+++ src/main/java/com/alkemy/ong/model/response/TestimonialResponse.java
@@ -0,0 +1,28 @@
+package com.alkemy.ong.model.response;
+
+import java.sql.Timestamp;
+import javax.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+public class TestimonialResponse {
+
+  @NotBlank
+  private Long testimonialId;
+
+  @NotBlank
+  private String name;
+
+  private String image;
+
+  @NotBlank
+  private String content;
+
+  private Timestamp timestamp;
+
+  private boolean softDelete;
+}
