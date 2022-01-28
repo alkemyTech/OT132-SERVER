9bc9627ade7e6d371f101997370a35e000f5b1a1
Facundo Castro <facucastrolp123@gmail.com>
2022-01-28 14:41:46 -0300

    OT132-65 changes in TestimonialController

--- src/main/java/com/alkemy/ong/controller/TestimonialController.java
+++ src/main/java/com/alkemy/ong/controller/TestimonialController.java
@@ -1,11 +1,10 @@
 package com.alkemy.ong.controller;
 
 import com.alkemy.ong.model.entity.Testimonial;
-import java.sql.Timestamp;
-import javax.persistence.Column;
-import org.hibernate.annotations.CreationTimestamp;
+import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("testimonials")
 public class TestimonialController {
 
+@PostMapping
+  public ResponseEntity<Object> create(@RequestBody @Valid Testimonial testimonial){
 
+  return null;
+  }
 }
