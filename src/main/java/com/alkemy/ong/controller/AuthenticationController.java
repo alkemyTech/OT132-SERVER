53f1bf330b456ace2436247b58781486ffb9f310
Facundo Besse <facundobesse@gmail.com>
2022-01-29 20:20:08 -0300

    OT132-25: details fixed

--- src/main/java/com/alkemy/ong/controller/AuthenticationController.java
+++ src/main/java/com/alkemy/ong/controller/AuthenticationController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ public class AuthenticationController {
   @Autowired
   private ILoginService loginService;
 
+  @PostMapping("/login")
   public ResponseEntity<AuthenticationResponse> login(
       @RequestBody @Valid AuthenticationRequest authenticationRequest)
       throws InvalidCredentialsException, MethodArgumentNotValidException, AuthenticationException {
