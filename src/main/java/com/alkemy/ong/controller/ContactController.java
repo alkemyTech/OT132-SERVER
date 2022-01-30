22a89ed7e33437ad305483d9f9b676826ec316ae
Agostina BC <79877363+AgosBC@users.noreply.github.com>
2022-01-30 19:51:37 -0300

    OT132-54: endpoint to create contacts (#30)

--- src/main/java/com/alkemy/ong/controller/ContactController.java
+++ src/main/java/com/alkemy/ong/controller/ContactController.java
@@ -1,16 +1,26 @@
 package com.alkemy.ong.controller;
 
+import com.alkemy.ong.model.request.CreateContactRequest;
+import com.alkemy.ong.model.response.ContactResponse;
 import com.alkemy.ong.model.response.ListContactResponse;
+import com.alkemy.ong.service.abstraction.ICreateContact;
 import com.alkemy.ong.service.abstraction.IGetContact;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/contacts")
 public class ContactController {
+
+  @Autowired
+  private ICreateContact createContact;
 
   @Autowired
   public IGetContact getContact;
@@ -20,4 +30,12 @@ public class ContactController {
     ListContactResponse contactResponse = getContact.list();
     return ResponseEntity.ok(contactResponse);
   }
+
+  @PostMapping
+  public ResponseEntity<ContactResponse> create(
+      @Valid @RequestBody CreateContactRequest contactRequest) {
+    ContactResponse contactResponse = createContact.create(contactRequest);
+    return ResponseEntity.status(HttpStatus.CREATED).body(contactResponse);
+  }
+
 }
