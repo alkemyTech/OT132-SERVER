5a64aba87968ed14293dc914037221a131a00e1b
Magali Kain <magali.kain@thoughtworks.com>
2022-01-31 21:20:55 +0100

    Adding integration test - Happy path

--- src/main/java/com/alkemy/ong/config/segurity/SecurityConfig.java
+++ src/main/java/com/alkemy/ong/config/segurity/SecurityConfig.java
@@ -57,6 +57,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .authorizeRequests()
         .antMatchers(HttpMethod.POST, "/auth/login")
         .permitAll()
+        .antMatchers(HttpMethod.POST, "/auth/register")
+        .permitAll()
         .antMatchers(HttpMethod.GET, "/organization/public")
         .permitAll()
         .antMatchers(HttpMethod.POST, "/activities")
