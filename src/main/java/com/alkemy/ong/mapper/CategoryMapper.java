bc0de3802c267d79a18cdc8f346f538b9fce0e8d
KharonBlue <facucastrolp123@gmailcom>
2022-02-01 23:12:31 -0300

    OT132-41 CategoryAttributes added and implement method in mapper

--- src/main/java/com/alkemy/ong/mapper/CategoryMapper.java
+++ src/main/java/com/alkemy/ong/mapper/CategoryMapper.java
@@ -1,13 +1,40 @@
 package com.alkemy.ong.mapper;
 
+import com.alkemy.ong.mapper.attribute.CategoryAttributes;
 import com.alkemy.ong.model.entity.Category;
 import com.alkemy.ong.model.response.CategoryResponse;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CategoryMapper {
+
+  public CategoryResponse map(Category category, CategoryAttributes... categoryAttributes) {
+    CategoryResponse categoryResponse = new CategoryResponse();
+    for (CategoryAttributes categoryAttribute : categoryAttributes) {
+      switch (categoryAttribute) {
+        case CATEGORY_ID:
+          categoryResponse.setIdCategory(category.getCategoryId());
+          break;
+        case NAME:
+          categoryResponse.setName(category.getName());
+          break;
+        case IMAGE:
+          categoryResponse.setImage(category.getImage());
+          break;
+        case DESCRIPTION:
+          categoryResponse.setDescription(category.getDescription());
+          break;
+        default:
+          throw new UnsupportedOperationException(
+              MessageFormat.format("Category attribute: {0} is unsupported",
+                  categoryAttribute));
+      }
+    }
+    return categoryResponse;
+  }
 
   public List<CategoryResponse> map(List<Category> categories) {
     List<CategoryResponse> categoryResponses = new ArrayList<>(categories.size());
