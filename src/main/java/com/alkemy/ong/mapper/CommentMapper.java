4128ac4fc445404cfae0b4edce7004ca1c6c6537
Leo Gomez <leonardo.e.gomez@gmail.com>
2022-02-03 12:49:12 -0300

    OT132-75: service and mappers added

--- src/main/java/com/alkemy/ong/mapper/CommentMapper.java
+++ src/main/java/com/alkemy/ong/mapper/CommentMapper.java
@@ -1,9 +1,23 @@
 package com.alkemy.ong.mapper;
 
+import com.alkemy.ong.model.entity.Comment;
+import com.alkemy.ong.model.response.CommentResponse;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CommentMapper {
 
+  public List<CommentResponse> map(List<Comment> comments){
+    List<CommentResponse> commentResponses = new ArrayList<>(comments.size());
+    for (Comment comment: comments){
+      CommentResponse commentResponse = new CommentResponse();
+      commentResponse.setBody(comment.getBody());
+      commentResponse.setTimestamp(comment.getTimestamp());
+      commentResponses.add(commentResponse);
+    }
+    return commentResponses;
+  }
 
 }
