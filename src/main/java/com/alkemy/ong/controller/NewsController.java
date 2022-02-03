3217d38000afb275280845bedac12839211464fd
intisolar <insolgar@gmail.com>
2022-02-03 01:31:03 -0300

    Trying annotation @ResponseStatus(HttpStatus.CREATED) to give 201 response

--- src/main/java/com/alkemy/ong/controller/NewsController.java
+++ src/main/java/com/alkemy/ong/controller/NewsController.java
@@ -5,20 +5,25 @@ import com.alkemy.ong.model.response.NewsResponse;
 import com.alkemy.ong.service.abstraction.ICreateNews;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("news")
 public class NewsController {
 
-  @Autowired private ICreateNews createNews;
+  @Autowired
+  private ICreateNews createNews;
 
   @PostMapping
-  public ResponseEntity<NewsResponse> create(@RequestBody @Valid CreateNewsRequest createNewsRequest) {
+  @ResponseStatus(HttpStatus.CREATED)
+  public ResponseEntity<NewsResponse> create(
+      @RequestBody @Valid CreateNewsRequest createNewsRequest) {
 
     NewsResponse newsResponse = createNews.create(createNewsRequest);
 
