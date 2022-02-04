65f99e6e24ee9141748cd257d58ed43f7f6d5d68
Alejo Galizzi <alejogalizzi@gmail.com>
2022-02-04 16:34:49 -0300

    OT132-62: Create service modified so that it log the exception in case the send function throw an email.

--- src/main/java/com/alkemy/ong/service/ContactService.java
+++ src/main/java/com/alkemy/ong/service/ContactService.java
@@ -1,5 +1,8 @@
 package com.alkemy.ong.service;
 
+import com.alkemy.ong.common.mail.EmailUtils;
+import com.alkemy.ong.common.mail.template.WelcomeEmailTemplate;
+import com.alkemy.ong.exception.ExternalServiceException;
 import com.alkemy.ong.mapper.ContactMapper;
 import com.alkemy.ong.model.entity.Contact;
 import com.alkemy.ong.model.request.CreateContactRequest;
@@ -10,17 +13,25 @@ import com.alkemy.ong.service.abstraction.ICreateContact;
 import com.alkemy.ong.service.abstraction.IGetContactDetails;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ContactService implements IGetContactDetails, ICreateContact {
 
+  private final Logger logger = LoggerFactory.getLogger(ExternalServiceException.class);
+
   @Autowired
   private IContactRepository contactRepository;
 
   @Autowired
   private ContactMapper contactMapper;
+
+  @Autowired
+  private EmailUtils emailUtils;
 
   @Override
   public ListContactResponse list() {
@@ -42,8 +53,15 @@ public class ContactService implements IGetContactDetails, ICreateContact {
   }
 
   @Override
-  public ContactResponse create(CreateContactRequest contactRequest) {
+  public ContactResponse create(CreateContactRequest contactRequest, String email)
+      throws ExternalServiceException {
     Contact contact = contactMapper.map(contactRequest);
+    WelcomeEmailTemplate template = new WelcomeEmailTemplate(email, contact.getName());
+    try {
+      emailUtils.send(template);
+    } catch (ExternalServiceException e) {
+      logger.error(e.getMessage());
+    }
     return contactMapper.map(contactRepository.save(contact));
   }
 
--- src/main/java/com/alkemy/ong/service/abstraction/ICreateContact.java
+++ src/main/java/com/alkemy/ong/service/abstraction/ICreateContact.java
@@ -1,10 +1,12 @@
 package com.alkemy.ong.service.abstraction;
 
+import com.alkemy.ong.exception.ExternalServiceException;
 import com.alkemy.ong.model.request.CreateContactRequest;
 import com.alkemy.ong.model.response.ContactResponse;
 
 public interface ICreateContact {
 
-  ContactResponse create(CreateContactRequest contactRequest);
+  ContactResponse create(CreateContactRequest contactRequest, String email)
+      throws ExternalServiceException;
 
 }
