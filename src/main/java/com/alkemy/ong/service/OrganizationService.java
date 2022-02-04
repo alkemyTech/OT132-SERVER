f0df66d7ed99ca5bbc1703ff271ce8652d6d12b4
Facundo Besse <facundobesse@gmail.com>
2022-02-04 16:04:42 -0300

    OT132-37: interface organization update created and implented in the service

--- src/main/java/com/alkemy/ong/service/OrganizationService.java
+++ src/main/java/com/alkemy/ong/service/OrganizationService.java
@@ -1,20 +1,23 @@
 package com.alkemy.ong.service;
 
+import com.alkemy.ong.exception.NotFoundException;
 import com.alkemy.ong.mapper.OrganizationMapper;
 import com.alkemy.ong.mapper.attribute.SlideAttributes;
 import com.alkemy.ong.model.entity.Organization;
 import com.alkemy.ong.model.entity.Slide;
+import com.alkemy.ong.model.request.OrganizationRequest;
 import com.alkemy.ong.model.response.OrganizationResponse;
 import com.alkemy.ong.repository.IOrganizationRepository;
 import com.alkemy.ong.repository.ISlideRepository;
 import com.alkemy.ong.service.abstraction.IGetOrganizationDetails;
+import com.alkemy.ong.service.abstraction.IOrganizationUpdate;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
-public class OrganizationService implements IGetOrganizationDetails {
+public class OrganizationService implements IGetOrganizationDetails, IOrganizationUpdate {
 
   @Autowired
   private IOrganizationRepository organizationRepository;
@@ -30,6 +33,31 @@ public class OrganizationService implements IGetOrganizationDetails {
     Organization organization = organizationRepository.findAll().get(0);
     List<Slide> slides = slideRepository.findAll(Sort.by(SlideAttributes.ORDER.getFieldName()));
     return mapper.map(organization, slides);
+  }
+
+
+  @Override
+  public void update(OrganizationRequest organizationRequest) throws NotFoundException{
+    Organization organization = getOrganization();
+    organization.setName(organizationRequest.getName());
+    organization.setImage(organizationRequest.getImage());
+    organization.setAddress(organizationRequest.getAddress());
+    organization.setPhone(organizationRequest.getPhone());
+    organization.setEmail(organizationRequest.getEmail());
+    organization.setWelcomeText(organizationRequest.getWelcomeText());
+    organization.setAboutUsText(organizationRequest.getAboutUsText());
+    organization.setFacebookUrl(organizationRequest.getFacebookUrl());
+    organization.setLinkedinUrl(organizationRequest.getLinkedinUrl());
+    organization.setInstagramUrl(organizationRequest.getInstagramUrl());
+    organizationRepository.save(organization);
+  }
+
+  private Organization getOrganization() throws NotFoundException{
+    List<Organization> organizations = organizationRepository.findAll();
+    if (organizations.isEmpty()) {
+      throw new NotFoundException("The requested resource could not be found.");
+    }
+    return organizations.get(0);
   }
 
 }
---
+++ src/main/java/com/alkemy/ong/service/abstraction/IOrganizationUpdate.java
@@ -0,0 +1,9 @@
+package com.alkemy.ong.service.abstraction;
+
+import com.alkemy.ong.model.request.OrganizationRequest;
+
+public interface IOrganizationUpdate {
+
+  void update(OrganizationRequest organizationRequest);
+
+}
