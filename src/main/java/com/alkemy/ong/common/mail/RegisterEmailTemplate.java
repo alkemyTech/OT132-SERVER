cce02a5dc74eb11ae6eb79a64d32cfd185042382
Facundo Besse <facundobesse@gmail.com>
2022-02-03 22:32:25 -0300

    OT132-29: requested changes completed

--- src/main/java/com/alkemy/ong/common/mail/RegisterEmailTemplate.java
+++ src/main/java/com/alkemy/ong/common/mail/RegisterEmailTemplate.java
@@ -7,28 +7,28 @@ import org.springframework.beans.factory.annotation.Value;
 public class RegisterEmailTemplate implements IContent, IMail {
 
   @Value("${organization.mail}")
-  private String EMAIL_TO;
+  private String emailTo;
 
   private static final String TYPE = "text/plain";
   private static final String SUBJECT = "Register Successfully";
   private static final String WELCOME_TEXT = "Welcome to {0}";
   private static final String CONTACT_US = "Contact us:";
-  private final String ORGANIZATION_NAME;
-  private final String ADDRESS;
-  private final Integer PHONE;
+  private final String organizationName;
+  private final String address;
+  private final Integer phone;
 
   public RegisterEmailTemplate(ContactUs contactUs) {
-    this.ORGANIZATION_NAME = contactUs.getOrganizationName();
-    this.ADDRESS = contactUs.getAddress();
-    this.PHONE = contactUs.getPhone();
+    this.organizationName = contactUs.getOrganizationName();
+    this.address = contactUs.getAddress();
+    this.phone = contactUs.getPhone();
   }
 
   @Override
   public String getBody() {
-    return MessageFormat.format(WELCOME_TEXT, ORGANIZATION_NAME)
+    return MessageFormat.format(WELCOME_TEXT, organizationName)
         + "\n" + CONTACT_US
-        + "\n" + "Address: " + ADDRESS
-        + "\n" + "Telephone: " + PHONE;
+        + "\n" + "Address: " + address
+        + "\n" + "Telephone: " + phone;
   }
 
   @Override
@@ -48,6 +48,6 @@ public class RegisterEmailTemplate implements IContent, IMail {
 
   @Override
   public String getTo() {
-    return EMAIL_TO;
+    return emailTo;
   }
 }
