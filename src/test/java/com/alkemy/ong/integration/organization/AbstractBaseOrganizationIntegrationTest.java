d72540cd931031cc2af75145c4d4fd1f578c7cee
AlejoGalizzi <81248632+AlejoGalizzi@users.noreply.github.com>
2022-02-10 19:38:44 -0300

    OT132-92: test endpoint /ong (#64)

---
+++ src/test/java/com/alkemy/ong/integration/organization/AbstractBaseOrganizationIntegrationTest.java
@@ -0,0 +1,50 @@
+package com.alkemy.ong.integration.organization;
+
+import static org.mockito.Mockito.when;
+
+import com.alkemy.ong.integration.common.AbstractBaseIntegrationTest;
+import com.alkemy.ong.model.entity.Organization;
+import com.alkemy.ong.repository.IOrganizationRepository;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Before;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+public abstract class AbstractBaseOrganizationIntegrationTest extends AbstractBaseIntegrationTest {
+
+  @MockBean
+  protected IOrganizationRepository organizationRepository;
+
+  @Before
+  public void checkFindMethod() {
+    when(organizationRepository.findAll()).thenReturn(buildOrganizationStub());
+  }
+
+  protected static final String PATH = "/organization/public";
+  protected static final String NAME = "Somos Mas";
+  protected static final String IMAGE = "http://foo.png";
+  protected static final String ADDRESS = "Street 123";
+  protected static final Integer PHONE = 456;
+  protected static final String EMAIL = "foo@gmail.com";
+  protected static final String WELCOME_TEXT = "Welcome";
+
+  protected List<Organization> buildOrganizationStub() {
+    List<Organization> organizations = new ArrayList<>();
+    organizations.add(createOrganizationStub());
+    return organizations;
+  }
+
+  protected Organization createOrganizationStub() {
+    Organization organization = new Organization();
+    organization.setName(NAME);
+    organization.setImage(IMAGE);
+    organization.setAddress(ADDRESS);
+    organization.setPhone(PHONE);
+    organization.setEmail(EMAIL);
+    organization.setWelcomeText(WELCOME_TEXT);
+    organization.setTimeStamp(Timestamp.from(Instant.now()));
+    return organization;
+  }
+}
---
+++ src/test/java/com/alkemy/ong/integration/organization/UpdateOrganizationIntegrationTest.java
@@ -0,0 +1,189 @@
+package com.alkemy.ong.integration.organization;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import com.alkemy.ong.config.segurity.RoleType;
+import com.alkemy.ong.exception.ErrorResponse;
+import com.alkemy.ong.model.entity.Organization;
+import com.alkemy.ong.model.request.UpdateOrganizationRequest;
+import com.alkemy.ong.model.response.OrganizationResponse;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UpdateOrganizationIntegrationTest extends AbstractBaseOrganizationIntegrationTest {
+
+  @Test
+  public void shouldUpdateOrganizationWithAdminRole() {
+    setAuthorizationHeaderBasedOn(RoleType.ADMIN);
+
+    when(organizationRepository.save(any(Organization.class))).thenReturn(createOrganizationStub());
+
+    UpdateOrganizationRequest updateOrganizationRequest = buildRequestPayload();
+
+    HttpEntity<UpdateOrganizationRequest> requestEntity = new HttpEntity<>(
+        updateOrganizationRequest, headers);
+
+    ResponseEntity<OrganizationResponse> response = restTemplate.exchange(
+        createURLWithPort(PATH), HttpMethod.POST, requestEntity, OrganizationResponse.class);
+
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+
+    OrganizationResponse organizationResponse = response.getBody();
+    assertNotNull(organizationResponse);
+    assertEquals(updateOrganizationRequest.getName(), organizationResponse.getName());
+    assertEquals(updateOrganizationRequest.getEmail(), organizationResponse.getEmail());
+    assertEquals(updateOrganizationRequest.getImage(), organizationResponse.getImage());
+    assertEquals(updateOrganizationRequest.getWelcomeText(), organizationResponse.getWelcomeText());
+  }
+
+  @Test
+  public void shouldReturnForbiddenWithUserRole() {
+    setAuthorizationHeaderBasedOn(RoleType.USER);
+
+    UpdateOrganizationRequest updateOrganizationRequest = buildRequestPayload();
+
+    HttpEntity<UpdateOrganizationRequest> requestEntity = new HttpEntity<>(
+        updateOrganizationRequest, headers);
+
+    ResponseEntity<ErrorResponse> response = restTemplate.exchange(createURLWithPort(PATH),
+        HttpMethod.POST, requestEntity, ErrorResponse.class);
+
+    assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
+    assertEquals(0, getMessageSizeError(response));
+  }
+
+  @Test
+  public void shouldReturnForbiddenWithNoRole() {
+
+    UpdateOrganizationRequest updateOrganizationRequest = buildRequestPayload();
+
+    HttpEntity<UpdateOrganizationRequest> requestEntity = new HttpEntity<>(
+        updateOrganizationRequest, headers);
+
+    ResponseEntity<ErrorResponse> response = restTemplate.exchange(createURLWithPort(PATH),
+        HttpMethod.POST, requestEntity, ErrorResponse.class);
+
+    assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
+    assertEquals(0, getMessageSizeError(response));
+  }
+
+  @Test
+  public void shouldReturnBadRequestWhenNameIsEmpty() {
+    setAuthorizationHeaderBasedOn(RoleType.ADMIN);
+
+    UpdateOrganizationRequest updateOrganizationRequest = buildRequestPayloadWithEmptyName();
+
+    HttpEntity<UpdateOrganizationRequest> requestEntity = new HttpEntity<>(
+        updateOrganizationRequest, headers);
+
+    ResponseEntity<ErrorResponse> response = restTemplate.exchange(createURLWithPort(PATH),
+        HttpMethod.POST, requestEntity, ErrorResponse.class);
+
+    assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    assertEquals(1, getMessageSizeError(response));
+    assertEquals("Name field can not be null or empty.", getFirstMessageError(response));
+  }
+
+  @Test
+  public void shouldReturnBadRequestWhenEmailIsEmpty() {
+    setAuthorizationHeaderBasedOn(RoleType.ADMIN);
+
+    UpdateOrganizationRequest updateOrganizationRequest = buildRequestPayloadWithEmptyEmail();
+
+    HttpEntity<UpdateOrganizationRequest> requestEntity = new HttpEntity<>(
+        updateOrganizationRequest, headers);
+
+    ResponseEntity<ErrorResponse> response = restTemplate.exchange(createURLWithPort(PATH),
+        HttpMethod.POST, requestEntity, ErrorResponse.class);
+
+    assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    assertEquals(1, getMessageSizeError(response));
+    assertEquals("Email field can not be null or empty.", getFirstMessageError(response));
+  }
+
+  @Test
+  public void shouldReturnBadRequestWhenImageIsEmpty() {
+    setAuthorizationHeaderBasedOn(RoleType.ADMIN);
+
+    UpdateOrganizationRequest updateOrganizationRequest = buildRequestPayloadWithEmptyImage();
+
+    HttpEntity<UpdateOrganizationRequest> requestEntity = new HttpEntity<>(
+        updateOrganizationRequest, headers);
+
+    ResponseEntity<ErrorResponse> response = restTemplate.exchange(createURLWithPort(PATH),
+        HttpMethod.POST, requestEntity, ErrorResponse.class);
+
+    assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    assertEquals(1, getMessageSizeError(response));
+    assertEquals("Image field can not be null or empty.", getFirstMessageError(response));
+  }
+
+  @Test
+  public void shouldReturnBadRequestWhenWelcomeTextIsEmpty() {
+    setAuthorizationHeaderBasedOn(RoleType.ADMIN);
+
+    UpdateOrganizationRequest updateOrganizationRequest = buildRequestPayloadWithEmptyWelcomeText();
+
+    HttpEntity<UpdateOrganizationRequest> requestEntity = new HttpEntity<>(
+        updateOrganizationRequest, headers);
+
+    ResponseEntity<ErrorResponse> response = restTemplate.exchange(createURLWithPort(PATH),
+        HttpMethod.POST, requestEntity, ErrorResponse.class);
+
+    assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    assertEquals(1, getMessageSizeError(response));
+    assertEquals("Welcome text field can not be null or empty.",
+        getFirstMessageError(response));
+  }
+
+  private UpdateOrganizationRequest buildRequestPayloadWithEmptyName() {
+    return buildRequestPayload(null, IMAGE, EMAIL, WELCOME_TEXT);
+  }
+
+  private UpdateOrganizationRequest buildRequestPayloadWithEmptyImage() {
+    return buildRequestPayload(NAME, null, EMAIL, WELCOME_TEXT);
+  }
+
+  private UpdateOrganizationRequest buildRequestPayloadWithEmptyEmail() {
+    return buildRequestPayload(NAME, IMAGE, null, WELCOME_TEXT);
+  }
+
+  private UpdateOrganizationRequest buildRequestPayloadWithEmptyWelcomeText() {
+    return buildRequestPayload(NAME, IMAGE, EMAIL, null);
+  }
+
+  private UpdateOrganizationRequest buildRequestPayload() {
+    return buildRequestPayload(NAME, IMAGE, EMAIL, WELCOME_TEXT);
+  }
+
+  private UpdateOrganizationRequest buildRequestPayload(String name, String image, String email,
+      String welcomeText) {
+    UpdateOrganizationRequest request = new UpdateOrganizationRequest();
+    request.setName(name);
+    request.setImage(image);
+    request.setEmail(email);
+    request.setWelcomeText(welcomeText);
+    request.setPhone(PHONE);
+    request.setAddress(ADDRESS);
+    return request;
+  }
+
+  private String getFirstMessageError(ResponseEntity<ErrorResponse> response) {
+    return response.getBody().getMessages().get(0);
+  }
+
+  private int getMessageSizeError(ResponseEntity<ErrorResponse> response) {
+    return response.getBody().getMessages().size();
+  }
+}
