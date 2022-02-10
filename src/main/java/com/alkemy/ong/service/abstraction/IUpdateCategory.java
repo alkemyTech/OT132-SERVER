aae889d4e410e90fec235a7e6973fc13cc0139d6
facubc <79551405+facubc@users.noreply.github.com>
2022-02-10 13:59:48 -0300

    OT132-43: update categories endpoint (#63)

---
+++ src/main/java/com/alkemy/ong/service/abstraction/IUpdateCategory.java
@@ -0,0 +1,10 @@
+package com.alkemy.ong.service.abstraction;
+
+import com.alkemy.ong.model.request.CreateCategoryRequest;
+import com.alkemy.ong.model.request.UpdateCategoryRequest;
+import com.alkemy.ong.model.response.CategoryResponse;
+
+public interface IUpdateCategory {
+
+  CategoryResponse update(Long id, UpdateCategoryRequest categoryRequest);
+}
