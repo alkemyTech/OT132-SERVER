9bbcbb6d6a019cfd501d0ee09522983ef0885dd1
Vladimir Morote <46836725+VladimirMorote@users.noreply.github.com>
2022-02-10 11:11:49 -0300

    Trying to config swagger

--- src/main/java/com/alkemy/ong/OngApplication.java
+++ src/main/java/com/alkemy/ong/OngApplication.java
@@ -1,9 +1,10 @@
 package com.alkemy.ong;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@OpenAPIDefinition
 @SpringBootApplication
 public class OngApplication {
 
