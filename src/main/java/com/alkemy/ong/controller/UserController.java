4a73b9ed6969b81f1b4acc88c5ae06b256c92113
intisolar <insolgar@gmail.com>
2022-02-11 15:13:20 -0300

    erased two more unused imports

--- src/main/java/com/alkemy/ong/controller/UserController.java
+++ src/main/java/com/alkemy/ong/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.alkemy.ong.controller;
 
 import com.alkemy.ong.exception.NotFoundException;
-import com.alkemy.ong.model.entity.User;
 import com.alkemy.ong.model.request.UpdateUserDetailsRequest;
 import com.alkemy.ong.model.response.ListUsersResponse;
 import com.alkemy.ong.model.response.UserResponse;
@@ -9,7 +8,6 @@ import com.alkemy.ong.service.abstraction.IDeleteUser;
 import com.alkemy.ong.service.abstraction.IGetUserDetails;
 import com.alkemy.ong.service.abstraction.IUpdateUserDetails;
 import javax.validation.Valid;
-import net.bytebuddy.asm.Advice.Return;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
