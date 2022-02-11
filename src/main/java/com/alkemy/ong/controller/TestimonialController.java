fc33c297680e7a58f015a5e28d345be150e8b78c
Franco Lorca <lorcafrancog@gmail.com>
2022-02-11 15:22:50 -0300

    OT132-83
    Created @GetMapping method in TestimonialController

--- src/main/java/com/alkemy/ong/controller/TestimonialController.java
+++ src/main/java/com/alkemy/ong/controller/TestimonialController.java
@@ -1,26 +1,38 @@
 package com.alkemy.ong.controller;
 
+import com.alkemy.ong.common.PaginatedResultsRetrieved;
 import com.alkemy.ong.model.request.CreateTestimonialRequest;
 import com.alkemy.ong.model.request.UpdateTestimonialRequest;
+import com.alkemy.ong.model.response.ListTestimonialResponse;
 import com.alkemy.ong.model.response.TestimonialResponse;
 import com.alkemy.ong.service.abstraction.ICreateTestimonial;
 import com.alkemy.ong.service.abstraction.IDeleteTestimonial;
+import com.alkemy.ong.service.abstraction.IGetTestimonialDetails;
 import com.alkemy.ong.service.abstraction.IUpdateTestimonial;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/testimonials")
 public class TestimonialController {
+
+  private static final String TESTIMONIAL_PATH = "/testimonials";
+
+  @Autowired
+  private IGetTestimonialDetails getTestimonialDetails;
 
   @Autowired
   private ICreateTestimonial createTestimonial;
@@ -30,6 +42,24 @@ public class TestimonialController {
 
   @Autowired
   private IUpdateTestimonial updateTestimonial;
+
+  @Autowired
+  private PaginatedResultsRetrieved resultsRetrieved;
+
+  @GetMapping
+  public ResponseEntity<ListTestimonialResponse> list(Pageable pageable,
+      UriComponentsBuilder uriBuilder, HttpServletResponse response) {
+    ListTestimonialResponse listTestimonialResponse = getTestimonialDetails.findAll(pageable);
+
+    resultsRetrieved.addLinkHeaderOnPagedResourceRetrieval(uriBuilder,
+        response,
+        TESTIMONIAL_PATH,
+        listTestimonialResponse.getPage(),
+        listTestimonialResponse.getTotalPages(),
+        listTestimonialResponse.getSize());
+    
+    return ResponseEntity.ok().body(listTestimonialResponse);
+  }
 
   @PostMapping
   public ResponseEntity<TestimonialResponse> create(
