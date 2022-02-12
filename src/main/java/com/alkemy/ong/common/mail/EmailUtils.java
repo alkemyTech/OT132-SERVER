ce0b5696c32f70e0d74c6b09faf63ad2dfe73296
KharonBlue <facucastrolp123@gmail.com>
2022-02-12 19:47:52 -0300

    OT132-30 update to sendgrid dependecy and changes in EmailsUtils

--- src/main/java/com/alkemy/ong/common/mail/EmailUtils.java
+++ src/main/java/com/alkemy/ong/common/mail/EmailUtils.java
@@ -22,29 +22,23 @@ public class EmailUtils {
   private String from;
 
   public void send(IMail mail) throws ExternalServiceException {
+    Content content = new Content();
+    content.setType(mail.getContent().getContentType());
+    content.setValue(mail.getContent().getBody());
+    Email fromEmail = new Email(from);
+    Email to = new Email(mail.getTo());
+    Mail sendgridMail = new Mail(fromEmail, mail.getSubject(), to, content);
+
     try {
-      Content content = new Content();
-      content.setType(mail.getContent().getContentType());
-      content.setValue(mail.getContent().getBody());
-
-      Mail sendgridMail = new Mail();
-      sendgridMail.setSubject(mail.getSubject());
-      sendgridMail.setFrom(new Email(from));
-      sendgridMail.setReplyTo(new Email(mail.getTo()));
-      sendgridMail.addContent(content);
-
       Request request = new Request();
       request.setMethod(Method.POST);
       request.setEndpoint("mail/send");
       request.setBody(sendgridMail.build());
-
       SendGrid sendGrid = new SendGrid(apiKey);
       Response response = sendGrid.api(request);
-
       if (response.getStatusCode() != 202) {
         throw new ExternalServiceException("Fails to send email: " + response.getBody());
       }
-
     } catch (IOException | RuntimeException e) {
       throw new ExternalServiceException(e.getMessage());
     }
