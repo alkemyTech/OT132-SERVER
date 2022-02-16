442dfacd76514c77cac547d5e4890a3d3dc45d7e
KharonBlue <facucastrolp123@gmail.com>
2022-02-16 18:59:37 -0300

    OT132-88 anotations added in TestimonialController

--- src/main/java/com/alkemy/ong/controller/TestimonialController.java
+++ src/main/java/com/alkemy/ong/controller/TestimonialController.java
@@ -1,20 +1,26 @@
 package com.alkemy.ong.controller;
 
 import com.alkemy.ong.common.PaginatedResultsRetrieved;
+import com.alkemy.ong.exception.ErrorResponse;
 import com.alkemy.ong.model.request.CreateTestimonialRequest;
 import com.alkemy.ong.model.request.UpdateTestimonialRequest;
+import com.alkemy.ong.model.response.AuthenticationResponse;
 import com.alkemy.ong.model.response.ListTestimonialResponse;
 import com.alkemy.ong.model.response.TestimonialResponse;
 import com.alkemy.ong.service.abstraction.ICreateTestimonial;
 import com.alkemy.ong.service.abstraction.IDeleteTestimonial;
 import com.alkemy.ong.service.abstraction.IGetTestimonialDetails;
 import com.alkemy.ong.service.abstraction.IUpdateTestimonial;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -46,7 +52,15 @@ public class TestimonialController {
   @Autowired
   private PaginatedResultsRetrieved resultsRetrieved;
 
-  @GetMapping
+  @GetMapping(produces = {"application/json"},
+      consumes = {"application/json"})
+  @ApiOperation(value = "Get pageable of testimonial", produces = "application/json",
+      consumes = "application/json")
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "OK - List of testimonial",
+          response = ListTestimonialResponse.class),
+      @ApiResponse(code = 403, message = "PERMISSION_DENIED - Forbidden.",
+          response = ErrorResponse.class)})
   public ResponseEntity<ListTestimonialResponse> list(Pageable pageable,
       UriComponentsBuilder uriBuilder, HttpServletResponse response) {
     ListTestimonialResponse listTestimonialResponse = getTestimonialDetails.findAll(pageable);
@@ -57,18 +71,39 @@ public class TestimonialController {
         listTestimonialResponse.getPage(),
         listTestimonialResponse.getTotalPages(),
         listTestimonialResponse.getSize());
-
     return ResponseEntity.ok().body(listTestimonialResponse);
   }
 
-  @PostMapping
+  @PostMapping(produces = {"application/json"},
+      consumes = {"application/json"})
+  @ApiOperation(value = "Create a testimonial", produces = "application/json",
+      consumes = "application/json")
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "OK - Testimonial created",
+          response = TestimonialResponse.class),
+      @ApiResponse(code = 403, message = "PERMISSION_DENIED - Forbidden.",
+          response = ErrorResponse.class),
+      @ApiResponse(code = 400, message = "INVALID_ARGUMENT - Bad request",
+          response = MethodArgumentNotValidException.class)})
   public ResponseEntity<TestimonialResponse> create(
       @RequestBody @Valid CreateTestimonialRequest createTestimonialRequest) {
     TestimonialResponse testimonialResponse = createTestimonial.create(createTestimonialRequest);
     return ResponseEntity.status(HttpStatus.CREATED).body(testimonialResponse);
   }
 
-  @PutMapping("{id}")
+  @PutMapping(value = "{id}", produces = {"application/json"},
+      consumes = {"application/json"})
+  @ApiOperation(value = "Update a testimonial", produces = "application/json",
+      consumes = "application/json")
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "OK - Updated Testimonial",
+          response = TestimonialResponse.class),
+      @ApiResponse(code = 403, message = "PERMISSION_DENIED - Forbidden.",
+          response = ErrorResponse.class),
+      @ApiResponse(code = 400, message = "INVALID_ARGUMENT - Bad request",
+          response = MethodArgumentNotValidException.class),
+      @ApiResponse(code = 404, message = "NOT_FOUND - Testimonial not found",
+          response = MethodArgumentNotValidException.class)})
   public ResponseEntity<TestimonialResponse> update(@PathVariable Long id,
       @RequestBody @Valid UpdateTestimonialRequest updateTestimonialRequest) {
     TestimonialResponse testimonialResponse = updateTestimonial.update(id,
@@ -76,7 +111,17 @@ public class TestimonialController {
     return ResponseEntity.status(HttpStatus.OK).body(testimonialResponse);
   }
 
-  @DeleteMapping("{id}")
+  @DeleteMapping(value = "{id}", produces = {"application/json"},
+      consumes = {"application/json"})
+  @ApiOperation(value = "Delete a testimonial", produces = "application/json",
+      consumes = "application/json")
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "OK - Testimonial deleted",
+          response = Void.class),
+      @ApiResponse(code = 403, message = "PERMISSION_DENIED - Forbidden.",
+          response = ErrorResponse.class),
+      @ApiResponse(code = 404, message = "NOT_FOUND - Testimonial not found",
+          response = MethodArgumentNotValidException.class)})
   public ResponseEntity<Void> delete(@PathVariable Long id) {
     deleteTestimonial.delete(id);
     return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
