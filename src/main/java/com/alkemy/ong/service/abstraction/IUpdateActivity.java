8c4e200fd40d8c202e6789cddab559ef2a843a36
AgostinaBarello <agostinabarello@gmail.com>
2022-02-15 21:04:29 -0300

    OT132-51 Update actiity, IupdateActivity, request, service y controller.

---
+++ src/main/java/com/alkemy/ong/service/abstraction/IUpdateActivity.java
@@ -0,0 +1,10 @@
+package com.alkemy.ong.service.abstraction;
+
+import com.alkemy.ong.model.request.UpdateActivityRequest;
+import com.alkemy.ong.model.response.ActivityResponse;
+
+public interface IUpdateActivity {
+
+  ActivityResponse update(long id, UpdateActivityRequest updateActivityRequest);
+  
+}
