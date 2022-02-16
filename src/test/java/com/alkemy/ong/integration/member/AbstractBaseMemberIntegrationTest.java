a99e6043ea0cded50eecfa6bd7a2ad52a361fa59
Alejo Galizzi <alejogalizzi@gmail.com>
2022-02-16 09:14:37 -0300

    OT132-91: Base of member integration test created.

---
+++ src/test/java/com/alkemy/ong/integration/member/AbstractBaseMemberIntegrationTest.java
@@ -0,0 +1,18 @@
+package com.alkemy.ong.integration.member;
+
+import com.alkemy.ong.integration.common.AbstractBaseIntegrationTest;
+
+public class AbstractBaseMemberIntegrationTest extends AbstractBaseIntegrationTest {
+
+  protected final static String MEMBER_PATH = "/members";
+  protected final static String NAME = "Joe";
+  protected final static String FACEBOOK_URL = "facebookUrl";
+  protected final static String INSTAGRAM_URL = "instagramUrl";
+  protected final static String LINKEDIN_URL = "linkedinUrl";
+  protected final static String IMAGE =
+      "https://cohorte-enero-835eb560.s3-us-east-2.amazonaws.com/image.txt";
+  protected final static String DESCRIPTION = "This is a description";
+  protected final static boolean SOFT_DELETE = false;
+
+
+}
