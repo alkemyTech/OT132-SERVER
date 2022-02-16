e83f2e0c7c88ced22dd370a69d4c81b44c8f6ca5
Franco Lorca <66887467+FrancoGL@users.noreply.github.com>
2022-02-16 14:31:05 -0300

    OT132-71: actualization's endpoint for Members (#77)

--- src/main/java/com/alkemy/ong/controller/MemberController.java
+++ src/main/java/com/alkemy/ong/controller/MemberController.java
@@ -2,10 +2,12 @@ package com.alkemy.ong.controller;
 
 import com.alkemy.ong.common.PaginatedResultsRetrieved;
 import com.alkemy.ong.model.request.CreateMemberRequest;
+import com.alkemy.ong.model.request.UpdateMemberRequest;
 import com.alkemy.ong.model.response.ListMembersResponse;
 import com.alkemy.ong.model.response.MemberResponse;
 import com.alkemy.ong.service.abstraction.ICreateMember;
 import com.alkemy.ong.service.abstraction.IGetMemberDetails;
+import com.alkemy.ong.service.abstraction.IUpdateMember;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +15,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +34,9 @@ public class MemberController {
 
   @Autowired
   private ICreateMember createMember;
+
+  @Autowired
+  private IUpdateMember updateMember;
 
   @Autowired
   private PaginatedResultsRetrieved resultsRetrieved;
@@ -52,5 +59,12 @@ public class MemberController {
       @Valid @RequestBody CreateMemberRequest createMemberRequest) {
     MemberResponse memberResponse = createMember.save(createMemberRequest);
     return ResponseEntity.status(HttpStatus.CREATED).body(memberResponse);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Void> update(@PathVariable Long id, @Valid @RequestBody
+      UpdateMemberRequest updateMemberRequest) {
+    updateMember.update(id, updateMemberRequest);
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
   }
 }
