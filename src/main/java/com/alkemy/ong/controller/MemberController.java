43e4cc33d267cddad7a52a0d66b1974cb74b8fb9
Alejo Galizzi <alejogalizzi@gmail.com>
2022-02-15 21:00:43 -0300

    OT132-82: Modify list method to return the member's list with the pagination.

--- src/main/java/com/alkemy/ong/controller/MemberController.java
+++ src/main/java/com/alkemy/ong/controller/MemberController.java
@@ -1,12 +1,15 @@
 package com.alkemy.ong.controller;
 
+import com.alkemy.ong.common.PaginatedResultsRetrieved;
 import com.alkemy.ong.model.request.CreateMemberRequest;
 import com.alkemy.ong.model.response.ListMembersResponse;
 import com.alkemy.ong.model.response.MemberResponse;
 import com.alkemy.ong.service.abstraction.ICreateMember;
 import com.alkemy.ong.service.abstraction.IGetMemberDetails;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +17,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/members")
 public class MemberController {
+
+  private static final String MEMBER_PATH = "/testimonials";
 
   @Autowired
   private IGetMemberDetails getMemberDetails;
@@ -25,10 +31,20 @@ public class MemberController {
   @Autowired
   private ICreateMember createMember;
 
+  @Autowired
+  private PaginatedResultsRetrieved resultsRetrieved;
+
   @GetMapping
-  public ResponseEntity<ListMembersResponse> list() {
-    ListMembersResponse listMembersResponse = getMemberDetails.findAll();
-    return ResponseEntity.ok().body(listMembersResponse);
+  public ResponseEntity<ListMembersResponse> list(Pageable pageable,
+      UriComponentsBuilder uriBuilder, HttpServletResponse response) {
+    ListMembersResponse memberResponses = getMemberDetails.findAll(pageable);
+    resultsRetrieved.addLinkHeaderOnPagedResourceRetrieval(uriBuilder,
+        response,
+        MEMBER_PATH,
+        memberResponses.getPage(),
+        memberResponses.getTotalPages(),
+        memberResponses.getSize());
+    return ResponseEntity.ok().body(memberResponses);
   }
 
   @PostMapping
