acf51dcfdb29772e04137eac8d70ffd170f6c7f2
KharonBlue <88806080+KharonBlue@users.noreply.github.com>
2022-02-17 08:30:28 -0300

    OT132-88: add documentation to testimonials (#80)

--- src/main/java/com/alkemy/ong/model/request/CreateTestimonialRequest.java
+++ src/main/java/com/alkemy/ong/model/request/CreateTestimonialRequest.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.model.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import javax.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,11 +10,19 @@ import lombok.Setter;
 public class CreateTestimonialRequest {
 
   @NotBlank(message = "Name cannot be empty or null.")
+  @ApiModelProperty(example = "Jane",
+      required = true,
+      position = 0)
   private String name;
 
+  @ApiModelProperty(example = "image.jpg",
+      position = 1)
   private String image;
 
   @NotBlank(message = "Content cannot be empty or null.")
+  @ApiModelProperty(example = "content",
+      required = true,
+      position = 3)
   private String content;
 
 }
