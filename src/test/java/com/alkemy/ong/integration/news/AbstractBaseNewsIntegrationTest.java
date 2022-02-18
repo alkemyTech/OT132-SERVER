714a52b8f0db513b06b44dd664870907194a14d7
intisolar <insolgar@gmail.com>
2022-02-18 01:08:11 -0300

    Added constructors to news and category entities and built tests and methods.

--- src/test/java/com/alkemy/ong/integration/news/AbstractBaseNewsIntegrationTest.java
+++ src/test/java/com/alkemy/ong/integration/news/AbstractBaseNewsIntegrationTest.java
@@ -3,26 +3,28 @@ package com.alkemy.ong.integration.news;
 
 import static org.junit.Assert.assertNotNull;
 
+import com.alkemy.ong.exception.ErrorResponse;
 import com.alkemy.ong.integration.common.AbstractBaseIntegrationTest;
 import com.alkemy.ong.model.entity.Category;
+import com.alkemy.ong.model.entity.News;
 import com.alkemy.ong.repository.ICategoryRepository;
-//import com.alkemy.ong.repository.INewsRepository;
+import com.alkemy.ong.repository.INewsRepository;
 import java.sql.Timestamp;
 import java.time.Instant;
-import org.junit.Before;
-import org.junit.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
 
 public abstract class AbstractBaseNewsIntegrationTest extends AbstractBaseIntegrationTest {
 
-//  @MockBean
-//  protected INewsRepository newsRepository;
+  @MockBean
+  protected INewsRepository newsRepository;
 
   @MockBean
   protected ICategoryRepository categoryRepository;
 
   protected static final String PATH = "/news";
   protected static final String NEWS_CATEGORY = "news";
+  protected static final Long NEWS_ID = 1L;
   protected static final String NAME = "Important News!";
   protected static final String TEXT = "We found the cure for clumsy programming. Yey!!";
   protected static final String IMAGE = "http://fake-url-image.com/this-is-not-a-pipe.jpg";
@@ -35,5 +37,14 @@ public abstract class AbstractBaseNewsIntegrationTest extends AbstractBaseIntegr
         null,
         Timestamp.from(Instant.now()),
         false);
+  }
+
+  protected News stubNews() {
+    return new News(NEWS_ID, NAME, TEXT, IMAGE, stubCategory(), false,
+        Timestamp.from(Instant.now()));
+  }
+
+  protected String getFirstMessageError(ResponseEntity<ErrorResponse> response) {
+    return response.getBody().getMessages().get(0);
   }
 }
--- src/test/java/com/alkemy/ong/integration/news/CreateNewsIntegrationTest.java
+++ src/test/java/com/alkemy/ong/integration/news/CreateNewsIntegrationTest.java
@@ -2,10 +2,13 @@ package com.alkemy.ong.integration.news;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 import com.alkemy.ong.config.segurity.RoleType;
+import com.alkemy.ong.exception.ErrorResponse;
+import com.alkemy.ong.model.entity.News;
 import com.alkemy.ong.model.request.CreateNewsRequest;
 import com.alkemy.ong.model.response.NewsResponse;
 import org.junit.Test;
@@ -30,26 +33,125 @@ public class CreateNewsIntegrationTest extends AbstractBaseNewsIntegrationTest {
     when(categoryRepository.findByNameIgnoreCase(eq(NEWS_CATEGORY)))
         .thenReturn(stubCategory());
 
+    when(newsRepository.save(any(News.class))).thenReturn(stubNews());
+
     CreateNewsRequest createNewsRequest = buildRequestPayLoad();
-
     HttpEntity<CreateNewsRequest> requestEntity = new HttpEntity<>(createNewsRequest, headers);
-
+//Esto no asigna otros valores que sí van en la response. Ver mañana
     ResponseEntity<NewsResponse> response = restTemplate.exchange(createURLWithPort(PATH),
         HttpMethod.POST, requestEntity, NewsResponse.class);
 
     assertEquals(HttpStatus.CREATED, response.getStatusCode());
 
+    NewsResponse newsResponse = response.getBody();
+    assertNotNull(newsResponse);
+
+    assertEquals(createNewsRequest.getName(), newsResponse.getName());
+    assertEquals(createNewsRequest.getImage(), newsResponse.getImage());
+    assertEquals(createNewsRequest.getText(), newsResponse.getText());
+    assertEquals(stubNews().getNewsId(), newsResponse.getNewsId());
+    assertEquals(stubCategory().getName(), newsResponse.getCategoryName());
+
+  }
+
+  @Test
+  public void shouldReturnForbiddenWhenRoleIsUser() {
+
+    setAuthorizationHeaderBasedOn(RoleType.USER);
+
+    CreateNewsRequest createNewsRequest = buildRequestPayLoad();
+
+    ResponseEntity<ErrorResponse> response = getErrorResponseEntity(createNewsRequest);
+
+    assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
+    assertEquals(403, response.getBody().getStatus());
+  }
+
+  @Test
+  public void shouldReturnForbiddenWhenAccessWithoutAuthentication() {
+    CreateNewsRequest createNewsRequest = buildRequestPayLoad();
+    ResponseEntity<ErrorResponse> response = getErrorResponseEntity(createNewsRequest);
+
+    assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
+    assertEquals(403, response.getBody().getStatus());
+  }
+
+  @Test
+  public void shouldReturnBadRequestWhenNameIsEmpty() {
+
+    setAuthorizationHeaderBasedOn(RoleType.ADMIN);
+
+    CreateNewsRequest createNewsRequest = buildRequestWithNullName();
+
+    ResponseEntity<ErrorResponse> response = getErrorResponseEntity(createNewsRequest);
+
+    assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    assertEquals(400, response.getBody().getStatus());
+    assertEquals("Name cannot be null or empty.", getFirstMessageError(response));
+
+  }
+
+  @Test
+  public void shouldReturnBadRequestWhenTextIsEmpty() {
+
+    setAuthorizationHeaderBasedOn(RoleType.ADMIN);
+
+    CreateNewsRequest createNewsRequest = buildRequestWithNullText();
+
+    ResponseEntity<ErrorResponse> response = getErrorResponseEntity(createNewsRequest);
+
+    assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    assertEquals(400, response.getBody().getStatus());
+    assertEquals("Text cannot be null or empty.", getFirstMessageError(response));
+
+  }
+
+  @Test
+  public void shouldReturnBadRequestWhenImageIsEmpty() {
+
+    setAuthorizationHeaderBasedOn(RoleType.ADMIN);
+
+    CreateNewsRequest createNewsRequest = buildRequestWithNullImage();
+
+    ResponseEntity<ErrorResponse> response = getErrorResponseEntity(createNewsRequest);
+
+    assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    assertEquals(400, response.getBody().getStatus());
+    assertEquals("Image cannot be null or empty.", getFirstMessageError(response));
+
+  }
+
+  private CreateNewsRequest buildRequestWithNullName() {
+    return buildRequestPayLoad(null, TEXT, IMAGE);
+  }
+
+  private CreateNewsRequest buildRequestWithNullText() {
+    return buildRequestPayLoad(NAME, null, IMAGE);
+  }
+
+  private CreateNewsRequest buildRequestWithNullImage() {
+    return buildRequestPayLoad(NAME, TEXT, null);
   }
 
   private CreateNewsRequest buildRequestPayLoad() {
+
     return buildRequestPayLoad(NAME, TEXT, IMAGE);
   }
 
-  private CreateNewsRequest buildRequestPayLoad(String name, String text, String image) {
-    CreateNewsRequest createNewsRequest = new CreateNewsRequest();
-    createNewsRequest.setName(name);
-    createNewsRequest.setText(text);
-    createNewsRequest.setImage(image);
-    return createNewsRequest;
+  private CreateNewsRequest buildRequestPayLoad(
+      String name, String text, String image) {
+    return new CreateNewsRequest(name, text, image);
+  }
+
+  private ResponseEntity<ErrorResponse> getErrorResponseEntity(
+      CreateNewsRequest createRequest) {
+
+    HttpEntity<CreateNewsRequest> request =
+        new HttpEntity<>(createRequest, headers);
+
+    return restTemplate.exchange(createURLWithPort(PATH),
+        HttpMethod.POST,
+        request,
+        ErrorResponse.class);
   }
 }
