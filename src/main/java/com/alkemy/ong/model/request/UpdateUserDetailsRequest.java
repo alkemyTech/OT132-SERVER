87d0c8be07b3aed99ceade73c04023631e291e5b
Alejo Galizzi <alejogalizzi@gmail.com>
2022-02-21 16:03:14 -0300

    Add user endpoints documentation.

--- src/main/java/com/alkemy/ong/model/request/UpdateUserDetailsRequest.java
+++ src/main/java/com/alkemy/ong/model/request/UpdateUserDetailsRequest.java
@@ -1,19 +1,32 @@
 package com.alkemy.ong.model.request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@ApiModel
 public class UpdateUserDetailsRequest {
 
+  @ApiModelProperty(example = "Jane",
+      position = 0)
   private String firstName;
 
+  @ApiModelProperty(example = "Doe",
+      position = 1)
   private String lastName;
 
+  @ApiModelProperty(example = "jane@mail.com",
+      position = 2)
   private String email;
 
+  @ApiModelProperty(example = "foo123",
+      position = 3)
   private String password;
 
+  @ApiModelProperty(example = "https://cohorte-enero-835eb560.s3.amazonaws.com/picture.png",
+      position = 4)
   private String photo;
 }
