4405deaa038b92731781e4e7cbb202ad03068a84
Alejo Galizzi <alejogalizzi@gmail.com>
2022-02-21 13:46:57 -0300

    Add comment documentation.

--- src/main/java/com/alkemy/ong/controller/ContactController.java
+++ src/main/java/com/alkemy/ong/controller/ContactController.java
@@ -1,10 +1,18 @@
 package com.alkemy.ong.controller;
 
+import com.alkemy.ong.exception.ErrorResponse;
 import com.alkemy.ong.model.request.CreateContactRequest;
 import com.alkemy.ong.model.response.ContactResponse;
+import com.alkemy.ong.model.response.ListCommentResponse;
 import com.alkemy.ong.model.response.ListContactResponse;
 import com.alkemy.ong.service.abstraction.ICreateContact;
 import com.alkemy.ong.service.abstraction.IGetContactDetails;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,10 +21,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/contacts")
+@Api(tags = "Contact Endpoints", value = "ContactEndpoints")
 public class ContactController {
 
   @Autowired
@@ -26,11 +36,41 @@ public class ContactController {
   public IGetContactDetails getContactDetails;
 
   @GetMapping
+  @ApiOperation(value = "Return the list of contacts")
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "OK - The list of contacts.",
+          response = ListCommentResponse.class),
+      @ApiResponse(code = 403, message = "PERMISSION_DENIED - Forbidden.",
+          response = ErrorResponse.class)})
+  @ApiImplicitParams(value = {
+      @ApiImplicitParam(name = "Authorization",
+          value = "Access Token",
+          required = true,
+          allowEmptyValue = false,
+          paramType = "header",
+          dataTypeClass =
+              String.class,
+          example =
+              "Bearer access_token")})
   public ResponseEntity<ListContactResponse> list() {
     return ResponseEntity.ok(getContactDetails.list());
   }
 
   @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  @ApiOperation(value = "Create a contact")
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "OK - The contact was successfully created"),
+      @ApiResponse(code = 400, message = "INVALID_ARGUMENT - Certain arguments "
+          + "cannot be empty or null."),
+      @ApiResponse(code = 403, message = "PERMISSION_DENIED - Forbidden.",
+          response = ErrorResponse.class)})
+  @ApiImplicitParam(name = "Authorization", value = "Access Token",
+      required = true,
+      allowEmptyValue = false,
+      paramType = "header",
+      dataTypeClass = String.class,
+      example = "Bearer access_token")
   public ResponseEntity<ContactResponse> create(
       @Valid @RequestBody CreateContactRequest contactRequest) {
     ContactResponse contactResponse = createContact.create(contactRequest);
--- src/main/java/com/alkemy/ong/model/response/ContactResponse.java
+++ src/main/java/com/alkemy/ong/model/response/ContactResponse.java
@@ -1,20 +1,39 @@
 package com.alkemy.ong.model.response;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@ApiModel
 public class ContactResponse {
 
+  @ApiModelProperty(example = "Id",
+      dataType = "long",
+      position = 0)
   private long contactId;
 
+  @ApiModelProperty(example = "Jane",
+      dataType = "string",
+      position = 1)
   private String name;
 
+  @ApiModelProperty(example = "12313412",
+      dataType = "integer",
+      position = 2)
   private Integer phone;
 
+  @ApiModelProperty(example = "jane@email.com",
+      dataType = "string",
+      position = 3)
   private String email;
 
+
+  @ApiModelProperty(example = "This is a message",
+      dataType = "string",
+      position = 4)
   private String message;
 
 }
