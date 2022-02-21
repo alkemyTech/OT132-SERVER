e369f4672749a1e33d4eb3b0c5d58b2c54b9c15d
Alejo Galizzi <alejogalizzi@gmail.com>
2022-02-21 15:03:31 -0300

    Add slide documentation.

--- src/main/java/com/alkemy/ong/model/response/SlideResponse.java
+++ src/main/java/com/alkemy/ong/model/response/SlideResponse.java
@@ -1,17 +1,30 @@
 package com.alkemy.ong.model.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@ApiModel
 public class SlideResponse {
 
+  @ApiModelProperty(example = "This is a comment",
+      dataType = "long",
+      position = 0)
   private Long slideId;
+  @ApiModelProperty(example = "https://cohorte-enero-835eb560.s3.amazonaws.com/name.png",
+      position = 1)
   private String imageUrl;
+  @ApiModelProperty(example = "This is a text",
+      position = 2)
   private String text;
+  @ApiModelProperty(example = "1",
+      dataType = "integer",
+      position = 3)
   private Integer order;
 
 }
