fbd59da5442a36f15fcfe859acab83e75970218f
Alejo Galizzi <alejogalizzi@gmail.com>
2022-02-22 01:50:11 -0300

    Add category endpoints documentation.

--- src/main/java/com/alkemy/ong/controller/CategoryController.java
+++ src/main/java/com/alkemy/ong/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.alkemy.ong.controller;
 
 import com.alkemy.ong.common.PaginatedResultsRetrieved;
+import com.alkemy.ong.exception.ErrorResponse;
 import com.alkemy.ong.exception.NotFoundException;
 import com.alkemy.ong.model.request.CreateCategoryRequest;
 import com.alkemy.ong.model.request.UpdateCategoryRequest;
@@ -10,6 +11,13 @@ import com.alkemy.ong.service.abstraction.ICreateCategory;
 import com.alkemy.ong.service.abstraction.IDeleteCategory;
 import com.alkemy.ong.service.abstraction.IGetCategoryDetails;
 import com.alkemy.ong.service.abstraction.IUpdateCategory;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ResponseHeader;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,11 +31,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/categories")
+@Api(tags = "Category Endpoints", value = "CategoryEndpoints")
 public class CategoryController {
 
   private static final String CATEGORIES_PATH = "/categories";
@@ -47,7 +57,35 @@ public class CategoryController {
   @Autowired
   private IDeleteCategory deleteCategory;
 
-  @GetMapping
+  @GetMapping(produces = {"application/json"})
+  @ApiOperation(value = "Return the list of categories by severous pages")
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "OK - The list of categories. The size of the page is the one"
+          + "passed in the parameters", response = ListCategoriesResponse.class, responseHeaders = {
+          @ResponseHeader(name = "Link",
+              description = "Link of the previous page and another for the next page",
+              response = String.class)}),
+      @ApiResponse(code = 403, message = "PERMISSION_DENIED - Forbidden.",
+          response = ErrorResponse.class)})
+  @ApiImplicitParams(value = {
+      @ApiImplicitParam(name = "page", value = "Page of the list",
+          required = true,
+          paramType = "query",
+          dataTypeClass = String.class,
+          example = "0"),
+      @ApiImplicitParam(name = "size",
+          value = "Size of the page",
+          required = false,
+          paramType = "query",
+          dataTypeClass = String.class,
+          example = "10"),
+      @ApiImplicitParam(name = "Authorization",
+          value = "Access Token",
+          required = true,
+          allowEmptyValue = false,
+          paramType = "header",
+          dataTypeClass = String.class,
+          example = "Bearer access_token")})
   public ResponseEntity<ListCategoriesResponse> list(Pageable pageable,
       UriComponentsBuilder uriBuilder, HttpServletResponse response) {
     ListCategoriesResponse listCategoriesResponse = getCategoryDetails.findAll(pageable);
@@ -62,26 +100,103 @@ public class CategoryController {
     return ResponseEntity.ok().body(listCategoriesResponse);
   }
 
-  @GetMapping("/{id}")
+  @GetMapping(value = "/{id}",produces = {"application/json"})
+  @ApiOperation(value = "Get a category's details.")
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "OK - The category was found and it return their details",
+          response = CategoryResponse.class),
+      @ApiResponse(code = 400, message = "INVALID_ARGUMENT - Certain arguments "
+          + "cannot be empty or null.",
+          response = ErrorResponse.class),
+      @ApiResponse(code = 403, message = "PERMISSION_DENIED - Forbidden.",
+          response = ErrorResponse.class)})
+  @ApiImplicitParam(name = "Authorization", value = "Access Token",
+      required = true,
+      allowEmptyValue = false,
+      paramType = "header",
+      dataTypeClass = String.class,
+      example = "Bearer access_token")
   public ResponseEntity<CategoryResponse> getBy(@PathVariable(value = "id") Long id) {
     CategoryResponse response = getCategoryDetails.getBy(id);
     return ResponseEntity.ok().body(response);
   }
 
-  @PostMapping
+  @PostMapping(produces = {"application/json"},
+      consumes = {"application/json"})
+  @ResponseStatus(HttpStatus.CREATED)
+  @ApiOperation(value = "Create a category and return it.")
+  @ApiResponses(value = {
+      @ApiResponse(code = 201, message = "CREATED - The category was successfully created",
+          response = CategoryResponse.class),
+      @ApiResponse(code = 400, message = "INVALID_ARGUMENT - Certain arguments "
+          + "cannot be empty or null.",
+          response = ErrorResponse.class),
+      @ApiResponse(code = 403, message = "PERMISSION_DENIED - Forbidden.",
+          response = ErrorResponse.class)})
+  @ApiImplicitParam(name = "Authorization", value = "Access Token",
+      required = true,
+      allowEmptyValue = false,
+      paramType = "header",
+      dataTypeClass = String.class,
+      example = "Bearer access_token")
   public ResponseEntity<CategoryResponse> create(
       @Valid @RequestBody CreateCategoryRequest createCategoryRequest) {
     CategoryResponse categoryResponse = createCategory.create(createCategoryRequest);
     return ResponseEntity.status(HttpStatus.CREATED).body(categoryResponse);
   }
 
-  @PutMapping("/{id}")
+  @PutMapping(value = "/{id}", produces = {"application/json"},
+      consumes = {"application/json"})
+  @ApiOperation(value = "Update a category passed by id.")
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "OK - The category was successfully updated"),
+      @ApiResponse(code = 400, message = "INVALID_ARGUMENT - Certain arguments "
+          + "cannot be empty or null.",
+          response = ErrorResponse.class),
+      @ApiResponse(code = 403, message = "PERMISSION_DENIED - Forbidden.",
+          response = ErrorResponse.class),
+      @ApiResponse(code = 404, message = "NOT_FOUND - Member not found.",
+          response = ErrorResponse.class)})
+  @ApiImplicitParams(value = {
+      @ApiImplicitParam(name = "id",
+          value = "Id of the category we want to update",
+          required = true, allowEmptyValue = false,
+          paramType = "path", dataTypeClass = String.class,
+          example = "1"),
+      @ApiImplicitParam(name = "Authorization",
+          value = "Access Token",
+          required = true,
+          allowEmptyValue = false,
+          paramType = "header",
+          dataTypeClass = String.class,
+          example = "Bearer access_token")})
   public ResponseEntity<CategoryResponse> update(@PathVariable("id") long id,
       @Valid @RequestBody UpdateCategoryRequest updateCategoryRequest) {
     return ResponseEntity.ok().body(updateCategory.update(id, updateCategoryRequest));
   }
 
-  @DeleteMapping("/{id}")
+  @DeleteMapping(value = "/{id}", produces = {"application/json"})
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @ApiOperation(value = "Delete a category passed by id.")
+  @ApiResponses(value = {
+      @ApiResponse(code = 204, message = "NO_CONTENT - The category was successfully deleted"),
+      @ApiResponse(code = 403, message = "PERMISSION_DENIED - Forbidden.",
+          response = ErrorResponse.class),
+      @ApiResponse(code = 404, message = "NOT_FOUND - Member not found.",
+          response = ErrorResponse.class)})
+  @ApiImplicitParams(value = {
+      @ApiImplicitParam(name = "id",
+          value = "Id of the category we want to delete",
+          required = true, allowEmptyValue = false,
+          paramType = "path", dataTypeClass = String.class,
+          example = "1"),
+      @ApiImplicitParam(name = "Authorization",
+          value = "Access Token",
+          required = true,
+          allowEmptyValue = false,
+          paramType = "header",
+          dataTypeClass = String.class,
+          example = "Bearer access_token")})
   public ResponseEntity<Void> delete(@PathVariable Long id) throws NotFoundException {
     deleteCategory.delete(id);
     return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
--- src/main/java/com/alkemy/ong/model/response/CategoryResponse.java
+++ src/main/java/com/alkemy/ong/model/response/CategoryResponse.java
@@ -1,19 +1,31 @@
 package com.alkemy.ong.model.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@ApiModel
 public class CategoryResponse {
 
+  @ApiModelProperty(example = "ID",
+      dataType = "long",
+      position = 0)
   private Long idCategory;
 
+  @ApiModelProperty(example = "Category number 1",
+      position = 1)
   private String name;
 
+  @ApiModelProperty(example = "This is a description",
+      position = 2)
   private String description;
 
+  @ApiModelProperty(example = "https://cohorte-enero-835eb560.s3.amazonaws.com/image.png",
+      position = 3)
   private String image;
 }
